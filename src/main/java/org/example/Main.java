a97261aa55cc16811f9817df27282ec94fbc17a7
= <=>
2023-09-01 10:51:03 +0900

    Create Student Project2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         // Press Opt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
+        System.out.println("Hello and welcome!!");
 
         Student s1 = new Student("22200733", "최동빈");
         System.out.println(s1.toString());
